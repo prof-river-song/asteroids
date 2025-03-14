d28887610d5163cca35efc86948415ddb43df49b
prof-river-song <lucia.kaku@gmail.com>
2025-03-14 14:44:02 -0500

    created circle class for game objects, and added framerate control to main

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
