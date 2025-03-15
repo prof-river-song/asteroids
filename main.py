f807fdeac856795990812f478dd6343d2d354132
prof-river-song <lucia.kaku@gmail.com>
2025-03-15 10:10:49 -0500

    created classes and methods to draw the player in the center of the game screen

--- main.py
+++ main.py
@@ -3,21 +3,27 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60
         dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
